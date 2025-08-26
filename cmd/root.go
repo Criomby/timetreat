bd306adaa56ab1e0daa6cbabaf9112fd02a998f3
Philippe <criomby@protonmail.com>
2025-08-26 13:30:22 +0200

    version 0.1.0

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:     "timetreat",
-	Version: "0.1.0-alpha",
+	Version: "0.1.0",
 	Short:   "Treat yourself to some tasty time tracking.",
 	Long: `  _   _                _                  _
  | | (_)              | |                | |
